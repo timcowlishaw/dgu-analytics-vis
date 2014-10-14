07930697b062dcc38338a3cd3ac8499e66f2f1fe
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-10-14 12:23:42 +0100

    resize pie slices on focus project

--- src/js/dgua/components/visitable_pie.js
+++ src/js/dgua/components/visitable_pie.js
@@ -8,6 +8,7 @@ var bind = require("../util/bind");
 var VisitablePie = function(app, visitables) {
   this._app = app;
   this._visitables = visitables;
+  this._app.registerMessageHandler("highlightPublisher", bind(this, this._onHighlightPublisher));
 };
 
 VisitablePie.prototype = {
@@ -15,12 +16,15 @@ VisitablePie.prototype = {
   _leftMargin: 50,
   _rightMargin: 150,
 
+  _highlightedRadiusCoeff: 1,
+  _notHighlightedRadiusCoeff: 0.3,
+
   render: function(selector) {
     this._element = slick.find(selector);  
     
     var width = this._element.offsetWidth - this._leftMargin - this._rightMargin;
     dom.setAttribute(this._element, "style", "height:" + width + "px;");
-    var radius = width / 2;
+    this._radius = width / 2;
     
     var svg = d3.select(this._element)
       .append("svg") 
@@ -29,22 +33,35 @@ VisitablePie.prototype = {
       .append("g")
         .attr("transform", "translate("+ (width / 2 + this._leftMargin) + "," + width / 2 + ")");
     
-    var layout = d3.layout.pie()
+    this._layout = d3.layout.pie()
       .sort(null)
       .value(function(d) { return d.visits(); });
     
-    var arc = d3.svg.arc()
-      .outerRadius(radius)
+    this._arc = d3.svg.arc()
+      .outerRadius(this._radius)
       .innerRadius(0);
 
-    svg.datum(this._visitables).selectAll("path").data(layout)
+    this._slice = svg.datum(this._visitables).selectAll("path").data(this._layout)
       .enter().append("path")
       .style("fill", function(d) { return d.data.color(); })
       .style("stroke", "#fff")
       .style("stroke-width", 1)
-      .attr("d", arc)
+      .attr("d", this._arc)
       .on("mouseover", bind(this, this._onMouseOver))
       .on("mouseout", bind(this, this._onMouseOut));
+  },
+
+  _onHighlightPublisher: function(publisher) {
+    if(publisher) {
+      this._arc.outerRadius(bind(this, function(d) {
+        var highlightRadius = this._highlightedRadiusCoeff * this._radius;
+        var notHighlightRadius = this._notHighlightedRadiusCoeff * this._radius;
+        return publisher.id() == d.data.id() ? highlightRadius : notHighlightRadius;
+      }));
+    } else {
+      this._arc.outerRadius(this._radius);
+    }
+    this._slice.attr("d", this._arc);
   },
 
   _onMouseOver: function(slice) {
