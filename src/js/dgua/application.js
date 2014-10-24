c24df517c89c9450de32c4fa2b9f629d241a6dd8
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2014-10-24 14:22:24 +0100

    move publishers and datasets setup from application into own root component

--- src/js/dgua/application.js
+++ src/js/dgua/application.js
@@ -4,8 +4,6 @@ var components = require("./components");
 var data = require("./data");
 var bind = require("./util/bind");
 var _ = require("underscore");
-var ColorKey = require("./util/color_key");
-var withVisitProportions = require("./util/with_visit_proportions");
 
 var Application = function(selector, dataPath) {
   this._selector = selector;
@@ -15,38 +13,12 @@ var Application = function(selector, dataPath) {
 
 Application.prototype = {
 
-  _nToDisplay: 5,
+  nToDisplay: 5,
 
   init: function() {
     data.Repository.loadDataSources(this._dataPath, bind(this, function(repo) {
 
-      var topPublishers = withVisitProportions(
-        this._topN(repo.getPublishersByVisits()),
-        repo.getTotalPublisherVisits()
-      );
- 
-      var topDatasets = withVisitProportions(
-        this._topN(repo.getDatasetsByVisits()),
-        repo.getTotalDatasetVisits()
-      );
-   
-      var publisherColorKey = new ColorKey(_.map(topPublishers, function(p) { return p.id(); }));
-
-      topPublishers = _.map(topPublishers, function(publisher) {
-        return publisherColorKey.withColor(publisher, publisher.id());
-      });
-
-      topDatasets = _.map(topDatasets, function(dataset) {
-        return publisherColorKey.withColor(dataset, dataset.publisher().id());
-      });
-
-      var datasetsList = new components.VisitableList(this, topDatasets);
-      var publishersList = new components.VisitableList(this, topPublishers);
-
-      var publishersPie = new components.VisitablePie(this, topPublishers);
-
-      var publishersColumn = new components.PublishersColumn(this, publishersPie, publishersList); 
-      var publishersDatasets = new components.PublishersDatasets(this, publishersColumn, datasetsList, publisherColorKey, this._nToDisplay);
+      var publishersDatasets = new components.PublishersDatasets(this, repo);
       
       publishersDatasets.render(this._selector);
     }));
--- src/js/dgua/components/publishers_datasets.js
+++ src/js/dgua/components/publishers_datasets.js
@@ -4,15 +4,16 @@ var render = require("../util/render");
 var fs = require("fs");
 var _ = require("underscore");
 var bind = require("../util/bind");
+var VisitableList = require("../components/visitable_list");
+var VisitablePie = require("../components/visitable_pie");
+var PublishersColumn = require("../components/publishers_column");
+var ColorKey = require("../util/color_key");
+var withVisitProportions = require("../util/with_visit_proportions");
 
-var PublishersDatasets = function(app, publishersColumn, datasetsColumn, publishersColorKey, topN) {
+var PublishersDatasets = function(app, repo) {
   this._app = app;
-  this._publishersColumn = publishersColumn;
-  this._datasetsColumn = datasetsColumn;
-  //FIXME this is a leak of responsibilities. Should these be instantiated here
-  //or passed in?
-  this._publishersColorKey = publishersColorKey;
-  this._topN = topN;
+  this._repo = repo;
+ 
   this._app.registerMessageHandler("selectPublisher", bind(this, this._onSelectPublisher));
 };
 
@@ -24,15 +25,48 @@ PublishersDatasets.prototype = {
   _datasetsColumnSelector: ".datasets",
 
   render: function(selector) {
+    var topPublishers = withVisitProportions(
+      this._topN(this._repo.getPublishersByVisits()),
+      this._repo.getTotalPublisherVisits()
+    );
+
+    var topDatasets = withVisitProportions(
+      this._topN(this._repo.getDatasetsByVisits()),
+      this._repo.getTotalDatasetVisits()
+    );
+
+    this._publisherColorKey = new ColorKey(_.map(topPublishers, function(p) { return p.id(); }));
+
+    
+    topPublishers = _.map(topPublishers, bind(this, function(publisher) {
+      return this._publisherColorKey.withColor(publisher, publisher.id());
+    }));
+    
+
+    topDatasets = _.map(topDatasets, bind(this, function(dataset) {
+      return this._publisherColorKey.withColor(dataset, dataset.publisher().id());
+    }));
+
+    this._datasetsColumn = new VisitableList(this._app, topDatasets);
+    
+    var publishersList = new VisitableList(this._app, topPublishers);
+    var publishersPie = new VisitablePie(this._app, topPublishers);
+    this._publishersColumn = new PublishersColumn(this._app, publishersPie, publishersList); 
+
     render.toSelector(this._template, selector);
+
     this._publishersColumn.render(selector + "  " + this._publishersColumnSelector);
     this._datasetsColumn.render(selector + "  " + this._datasetsColumnSelector);
   },
 
   _onSelectPublisher: function(publisher) {
-    this._datasetsColumn.update(_.map(_.first(publisher.datasets(), this._topN), bind(this, function(dataset) {
-      return this._publishersColorKey.withColor(dataset, dataset.publisher().id()); 
+    this._datasetsColumn.update(_.map(this._topN(publisher.datasets()), bind(this, function(dataset) {
+      return this._publisherColorKey.withColor(dataset, dataset.publisher().id()); 
     })));
   },
+
+  _topN: function(collection) {
+    return _.first(collection, this._app.nToDisplay);
+  }
 };
 module.exports = PublishersDatasets;
